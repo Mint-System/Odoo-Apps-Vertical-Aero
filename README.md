cda779f5815ffc006f4b4f3b6a2c315a875c1b90
janikvonrotz <contact@janikvonrotz.ch>
2022-07-14 12:02:25 +0200

    init

---
+++ README.md
@@ -0,0 +1,17 @@
+# Odoo Apps: Aero
+
+Collection of aero related modules.
+
+## Usage
+
+Clone module into Odoo addon directory.
+
+```bash
+git clone git@github.com:mint-system/odoo-apps-vertical-aero.git ./addons/vertical_aero
+```
+
+## Available modules
+
+| Module | Summary |
+| --- | --- |
+| [aero_base](aero_base) |         Base module of the aero vertical integration apps. |
---
+++ aero_base/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "Aero Base",
+    "summary": """
+        Base module of the aero vertical integration apps.
+    """,
+    "author": "Mint System GmbH, Odoo Community Association (OCA)",
+    "website": "https://www.mint-system.ch",
+    "category": "Aero",
+    "version": "15.0.1.0.0",
+    "license": "OPL-1",
+    "depends": ["contacts", "product"],
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+    "images": ["images/screen.png"],
+}
